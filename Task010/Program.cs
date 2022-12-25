ff47d2deb8d847f1124db87c2b0e32f5b114e321
Svetlana Ignatchik <lightbug89@gmail.com>
2022-12-25 22:45:52 +0300

    Task 010

---
+++ Task010/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число
+// и на выходе показывает вторую цифру этого числа.
+
+Console.Clear ();
+
+Console.Write ("Введите трехзначное число: ");
+int numberA = int.Parse(Console.ReadLine()!);
+
+int digitIndex = 1;
+int digit  = numberA.ToString()[digitIndex] - '0';
+
+Console.WriteLine ($"Вторая цифра введенного числа {digit}");
+
+
+
