3aa417bd09c3826746e265510557e895d97c704a
Svetlana Ignatchik <lightbug89@gmail.com>
2022-12-25 23:14:11 +0300

    Task 013

--- Task010/Program.cs
+++ Task010/Program.cs
@@ -5,8 +5,8 @@ Console.Clear ();
 
 Console.Write ("Введите трехзначное число: ");
 int numberA = int.Parse(Console.ReadLine()!);
-
 int digitIndex = 1;
+
 int digit  = numberA.ToString()[digitIndex] - '0';
 
 Console.WriteLine ($"Вторая цифра этого числа {digit}");
---
+++ Task013/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа
+// или сообщает, что третьей цифры нет. 
+
+Console.Clear ();
+
+Console.Write ("Введите число: ");
+int numberA = int.Parse(Console.ReadLine()!);
+int digitIndex = 2;
+
+if ( numberA > 100 && numberA < 1000)
+{
+  int digit  = numberA.ToString()[digitIndex] - '0';
+  Console.WriteLine ($"Третья цифра этого числа {digit}");
+}
+else 
+{
+  Console.WriteLine($"Третьей цифры нет");
+}
